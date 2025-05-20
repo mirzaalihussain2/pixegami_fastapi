e1f57a54c5c8b872b2964ac59abb30ee909994c0
mirzaalihussain2 <mirzaalihussain2@gmail.com>
2025-05-20 21:48:03 +0100

    Introducing exception handling

--- main.py
+++ main.py
@@ -13,7 +13,13 @@ def create_item(item: str):
     items.append(item)
     return items
 
+@app.get("/items")
+def list_items(limit: int = 10):
+    return items[0:limit]
+
 @app.get("/items/{item_id}")
 def get_item(item_id: int) -> str:
-    item = items[item_id]
-    return item
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
